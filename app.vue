52c45529db4dcfaa3f3aadf56f59359307ba38bb
Pavel Gonzales <gonzales_pi@itms.tech>
2022-12-19 18:03:53 +0300

    Тест переменных в наксте

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Server var (на клиенте потрется и станет недоступна): {{ config.serverVar }}
+    <br>
+    Public var: {{ config.public.publicVar }}
+  </div>
+</template>
+
+<script setup>
+const config = useRuntimeConfig();
+
+const envVariable = config.apiSecret;
+
+console.log('Server var', config.serverVar)
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  runtimeConfig: {
+    // Private keys are only available on the server
+    serverVar: process.env.SERVER_ONLY_VAR,
+    // Public keys that are exposed to the client
+    public: {
+      publicVar: process.env.PUBLIC_VAR
+    }
+  }
+})
